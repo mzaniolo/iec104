ae83f118089d7a2b8f0fac02828c848d7a13ca27
Matheus Zaniolo <mgzaniolo@gmail.com>
2025-09-13 12:01:28 +0200

    feat: Add default to config

--- src/config.rs
+++ src/config.rs
@@ -5,25 +5,27 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct ProtocolConfig {
 	/// The period between sending test frames. The default is 20 seconds.
-	#[serde(with = "humantime_serde")]
+	#[serde(with = "humantime_serde", default = "default_duration::<20>")]
 	pub t3: Duration,
 	/// The timeout after which the station must acknowledge receipt with
 	/// S-frames. The default is 10 seconds.
-	#[serde(with = "humantime_serde")]
+	#[serde(with = "humantime_serde", default = "default_duration::<10>")]
 	pub t2: Duration,
 	/// The timeout for considering the connection to be non-functional and
 	/// close it. The default is 15 seconds.
-	#[serde(with = "humantime_serde")]
+	#[serde(with = "humantime_serde", default = "default_duration::<15>")]
 	pub t1: Duration,
 	/// The period for connections attempts. The default is 10 second.
-	#[serde(with = "humantime_serde")]
+	#[serde(with = "humantime_serde", default = "default_duration::<10>")]
 	pub t0: Duration,
 	/// Maximum number of sent and unacknowledged ASDUs. Default is 12.
+	#[serde(default = "default_number::<12>")]
 	pub k: u16,
 	/// Latest acknowledge after receiving w I format APDUs. Default is 8
+	#[serde(default = "default_number::<8>")]
 	pub w: u16,
-
-	pub originator_address: u16,
+	/// The originator address for the IEC 104 connection.
+	pub originator_address: u8,
 }
 
 /// The client TLS configuration
@@ -32,13 +34,16 @@ pub struct TlsClientConfig {
 	/// Path to the client key; if not specified, it will be assumed
 	/// that the server is configured not to verify client
 	/// certificates.
+	#[serde(default)]
 	pub client_key: Option<PathBuf>,
 	/// Path to the client certificate; if not specified, it will be
 	/// assumed that the server is configured not to verify client
 	/// certificates.
+	#[serde(default)]
 	pub client_certificate: Option<PathBuf>,
 	/// Path to the server certificate; if not specified, the host's
 	/// CA will be used to verify the server.
+	#[serde(default)]
 	pub server_certificate: Option<PathBuf>,
 	/// Whether to verify the server's certificates.
 	///
@@ -56,8 +61,10 @@ pub struct ClientConfig {
 	/// The port of the server.
 	pub port: u16,
 	/// The protocol configuration.
+	#[serde(default)]
 	pub protocol: ProtocolConfig,
 	/// The TLS configuration.
+	#[serde(default)]
 	pub tls: Option<TlsClientConfig>,
 }
 
@@ -84,4 +91,12 @@ impl Default for ClientConfig {
 			tls: None,
 		}
 	}
+}
+
+const fn default_number<const N: u16>() -> u16 {
+	N
+}
+
+const fn default_duration<const N: u64>() -> Duration {
+	Duration::from_secs(N)
 }
