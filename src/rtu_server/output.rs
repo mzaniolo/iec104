86805001e7891b1a2bd6919bcdd139c9adc7d8be
Matheus Zaniolo <mgzaniolo@gmail.com>
2026-04-09 20:11:31 +0200

    feat: Add more point values support

--- src/rtu_server/output.rs
+++ src/rtu_server/output.rs
@@ -8,8 +8,10 @@ use crate::{
 	asdu::Asdu,
 	cot::Cot,
 	types::{
-		FromBytes, GenericObject, InformationObjects, MBoNa1, MDpNa1, MMeNa1, MMeNb1, MMeNc1,
-		MSpNa1, MStNa1, ToBytes,
+		FromBytes, GenericObject, InformationObjects, MBoNa1, MBoTb1, MDpNa1, MDpTa1, MDpTb1,
+		MEpTa1, MEpTb1, MEpTc1, MEpTd1, MEpTe1, MEpTf1, MItNa1, MItTb1, MMeNa1, MMeNb1, MMeNc1,
+		MMeNd1, MMeTa1, MMeTb1, MMeTc1, MMeTd1, MMeTe1, MMeTf1, MPsNa1, MSpNa1, MSpTa1, MSpTb1,
+		MStNa1, MStTa1, MStTb1, ToBytes,
 	},
 	types_id::TypeId,
 };
@@ -17,17 +19,16 @@ use crate::{
 /// Maximum number of information objects per ASDU (7-bit count field).
 pub(super) const MAX_OBJECTS_PER_ASDU: usize = 127;
 
-fn sort_points_by_ioa<T>(v: &mut [(u32, T)]) {
-	v.sort_by_key(|(ioa, _)| *ioa);
-}
-
-fn push_interrogation_chunks<T: Clone + FromBytes + ToBytes + Default>(
+fn sort_and_push_interrogation_chunks<T>(
 	out: &mut Vec<Asdu>,
 	ca: u16,
-	objs: &[(u32, T)],
+	objs: &mut [(u32, T)],
 	type_id: TypeId,
-	wrap: impl Fn(Vec<GenericObject<T>>) -> InformationObjects,
-) {
+) where
+	T: Clone + FromBytes + ToBytes + Default,
+	InformationObjects: From<Vec<GenericObject<T>>>,
+{
+	objs.sort_by_key(|(ioa, _)| *ioa);
 	for chunk in objs.chunks(MAX_OBJECTS_PER_ASDU) {
 		let chunk_objs: Vec<_> = chunk
 			.iter()
@@ -41,7 +42,7 @@ fn push_interrogation_chunks<T: Clone + FromBytes + ToBytes + Default>(
 			sequence: false,
 			test: false,
 			negative: false,
-			information_objects: wrap(chunk_objs),
+			information_objects: chunk_objs.into(),
 		});
 	}
 }
@@ -55,147 +56,130 @@ pub(super) fn monitoring_asdu_requested(address: PointAddress, value: &PointValu
 }
 
 pub(super) fn spontaneous_asdu(address: PointAddress, value: &PointValue) -> Asdu {
-	let ca = address.common_address;
-	let ioa = address.information_object_address;
-	match value {
-		PointValue::MSpNa1(m) => Asdu {
-			type_id: TypeId::M_SP_NA_1,
-			cot: Cot::SpontaneousData,
-			originator_address: 0,
-			address_field: ca,
-			sequence: false,
-			test: false,
-			negative: false,
-			information_objects: InformationObjects::MSpNa1(vec![GenericObject {
-				address: ioa,
-				object: m.clone(),
-			}]),
-		},
-		PointValue::MDpNa1(m) => Asdu {
-			type_id: TypeId::M_DP_NA_1,
-			cot: Cot::SpontaneousData,
-			originator_address: 0,
-			address_field: ca,
-			sequence: false,
-			test: false,
-			negative: false,
-			information_objects: InformationObjects::MDpNa1(vec![GenericObject {
-				address: ioa,
-				object: m.clone(),
-			}]),
-		},
-		PointValue::MStNa1(m) => Asdu {
-			type_id: TypeId::M_ST_NA_1,
-			cot: Cot::SpontaneousData,
-			originator_address: 0,
-			address_field: ca,
-			sequence: false,
-			test: false,
-			negative: false,
-			information_objects: InformationObjects::MStNa1(vec![GenericObject {
-				address: ioa,
-				object: m.clone(),
-			}]),
-		},
-		PointValue::MBoNa1(m) => Asdu {
-			type_id: TypeId::M_BO_NA_1,
-			cot: Cot::SpontaneousData,
-			originator_address: 0,
-			address_field: ca,
-			sequence: false,
-			test: false,
-			negative: false,
-			information_objects: InformationObjects::MBoNa1(vec![GenericObject {
-				address: ioa,
-				object: m.clone(),
-			}]),
-		},
-		PointValue::MMeNa1(m) => Asdu {
-			type_id: TypeId::M_ME_NA_1,
-			cot: Cot::SpontaneousData,
-			originator_address: 0,
-			address_field: ca,
-			sequence: false,
-			test: false,
-			negative: false,
-			information_objects: InformationObjects::MMeNa1(vec![GenericObject {
-				address: ioa,
-				object: m.clone(),
-			}]),
-		},
-		PointValue::MMeNb1(m) => Asdu {
-			type_id: TypeId::M_ME_NB_1,
-			cot: Cot::SpontaneousData,
-			originator_address: 0,
-			address_field: ca,
-			sequence: false,
-			test: false,
-			negative: false,
-			information_objects: InformationObjects::MMeNb1(vec![GenericObject {
-				address: ioa,
-				object: m.clone(),
-			}]),
-		},
-		PointValue::MMeNc1(m) => Asdu {
-			type_id: TypeId::M_ME_NC_1,
-			cot: Cot::SpontaneousData,
-			originator_address: 0,
-			address_field: ca,
-			sequence: false,
-			test: false,
-			negative: false,
-			information_objects: InformationObjects::MMeNc1(vec![GenericObject {
-				address: ioa,
-				object: m.clone(),
-			}]),
-		},
+	let (type_id, information_objects) =
+		value.single_object_information_objects(address.information_object_address);
+	Asdu {
+		type_id,
+		cot: Cot::SpontaneousData,
+		originator_address: 0,
+		address_field: address.common_address,
+		sequence: false,
+		test: false,
+		negative: false,
+		information_objects,
 	}
 }
 
+/// General-interrogation data ASDUs for one common address. Buckets must match
+/// [`PointValue`] variants (Type IDs 1–40 monitoring range).
+#[allow(clippy::too_many_lines)]
+#[must_use]
 pub(super) fn interrogation_data_asdus(
 	ca: u16,
 	model: &HashMap<PointAddress, PointValue>,
 ) -> Vec<Asdu> {
 	let mut out = Vec::new();
 
-	let mut sp: Vec<(u32, MSpNa1)> = Vec::new();
-	let mut dp: Vec<(u32, MDpNa1)> = Vec::new();
-	let mut st: Vec<(u32, MStNa1)> = Vec::new();
-	let mut bo: Vec<(u32, MBoNa1)> = Vec::new();
-	let mut mna: Vec<(u32, MMeNa1)> = Vec::new();
-	let mut mnb: Vec<(u32, MMeNb1)> = Vec::new();
-	let mut mnc: Vec<(u32, MMeNc1)> = Vec::new();
+	let mut m_sp_na: Vec<(u32, MSpNa1)> = Vec::new();
+	let mut m_sp_ta: Vec<(u32, MSpTa1)> = Vec::new();
+	let mut m_dp_na: Vec<(u32, MDpNa1)> = Vec::new();
+	let mut m_dp_ta: Vec<(u32, MDpTa1)> = Vec::new();
+	let mut m_st_na: Vec<(u32, MStNa1)> = Vec::new();
+	let mut m_st_ta: Vec<(u32, MStTa1)> = Vec::new();
+	let mut m_bo_na: Vec<(u32, MBoNa1)> = Vec::new();
+	let mut m_me_na: Vec<(u32, MMeNa1)> = Vec::new();
+	let mut m_me_ta: Vec<(u32, MMeTa1)> = Vec::new();
+	let mut m_me_nb: Vec<(u32, MMeNb1)> = Vec::new();
+	let mut m_me_tb: Vec<(u32, MMeTb1)> = Vec::new();
+	let mut m_me_nc: Vec<(u32, MMeNc1)> = Vec::new();
+	let mut m_me_tc: Vec<(u32, MMeTc1)> = Vec::new();
+	let mut m_it_na: Vec<(u32, MItNa1)> = Vec::new();
+	let mut m_ep_ta: Vec<(u32, MEpTa1)> = Vec::new();
+	let mut m_ep_tb: Vec<(u32, MEpTb1)> = Vec::new();
+	let mut m_ep_tc: Vec<(u32, MEpTc1)> = Vec::new();
+	let mut m_ps_na: Vec<(u32, MPsNa1)> = Vec::new();
+	let mut m_me_nd: Vec<(u32, MMeNd1)> = Vec::new();
+	let mut m_sp_tb: Vec<(u32, MSpTb1)> = Vec::new();
+	let mut m_dp_tb: Vec<(u32, MDpTb1)> = Vec::new();
+	let mut m_st_tb: Vec<(u32, MStTb1)> = Vec::new();
+	let mut m_bo_tb: Vec<(u32, MBoTb1)> = Vec::new();
+	let mut m_me_td: Vec<(u32, MMeTd1)> = Vec::new();
+	let mut m_me_te: Vec<(u32, MMeTe1)> = Vec::new();
+	let mut m_me_tf: Vec<(u32, MMeTf1)> = Vec::new();
+	let mut m_it_tb: Vec<(u32, MItTb1)> = Vec::new();
+	let mut m_ep_td: Vec<(u32, MEpTd1)> = Vec::new();
+	let mut m_ep_te: Vec<(u32, MEpTe1)> = Vec::new();
+	let mut m_ep_tf: Vec<(u32, MEpTf1)> = Vec::new();
+
 	for (addr, v) in model.iter() {
 		if addr.common_address != ca {
 			continue;
 		}
 		let ioa = addr.information_object_address;
 		match v {
-			PointValue::MSpNa1(m) => sp.push((ioa, m.clone())),
-			PointValue::MDpNa1(m) => dp.push((ioa, m.clone())),
-			PointValue::MStNa1(m) => st.push((ioa, m.clone())),
-			PointValue::MBoNa1(m) => bo.push((ioa, m.clone())),
-			PointValue::MMeNa1(m) => mna.push((ioa, m.clone())),
-			PointValue::MMeNb1(m) => mnb.push((ioa, m.clone())),
-			PointValue::MMeNc1(m) => mnc.push((ioa, m.clone())),
+			PointValue::MSpNa1(m) => m_sp_na.push((ioa, m.clone())),
+			PointValue::MSpTa1(m) => m_sp_ta.push((ioa, m.clone())),
+			PointValue::MDpNa1(m) => m_dp_na.push((ioa, m.clone())),
+			PointValue::MDpTa1(m) => m_dp_ta.push((ioa, m.clone())),
+			PointValue::MStNa1(m) => m_st_na.push((ioa, m.clone())),
+			PointValue::MStTa1(m) => m_st_ta.push((ioa, m.clone())),
+			PointValue::MBoNa1(m) => m_bo_na.push((ioa, m.clone())),
+			PointValue::MMeNa1(m) => m_me_na.push((ioa, m.clone())),
+			PointValue::MMeTa1(m) => m_me_ta.push((ioa, m.clone())),
+			PointValue::MMeNb1(m) => m_me_nb.push((ioa, m.clone())),
+			PointValue::MMeTb1(m) => m_me_tb.push((ioa, m.clone())),
+			PointValue::MMeNc1(m) => m_me_nc.push((ioa, m.clone())),
+			PointValue::MMeTc1(m) => m_me_tc.push((ioa, m.clone())),
+			PointValue::MItNa1(m) => m_it_na.push((ioa, m.clone())),
+			PointValue::MEpTa1(m) => m_ep_ta.push((ioa, m.clone())),
+			PointValue::MEpTb1(m) => m_ep_tb.push((ioa, m.clone())),
+			PointValue::MEpTc1(m) => m_ep_tc.push((ioa, m.clone())),
+			PointValue::MPsNa1(m) => m_ps_na.push((ioa, m.clone())),
+			PointValue::MMeNd1(m) => m_me_nd.push((ioa, m.clone())),
+			PointValue::MSpTb1(m) => m_sp_tb.push((ioa, m.clone())),
+			PointValue::MDpTb1(m) => m_dp_tb.push((ioa, m.clone())),
+			PointValue::MStTb1(m) => m_st_tb.push((ioa, m.clone())),
+			PointValue::MBoTb1(m) => m_bo_tb.push((ioa, m.clone())),
+			PointValue::MMeTd1(m) => m_me_td.push((ioa, m.clone())),
+			PointValue::MMeTe1(m) => m_me_te.push((ioa, m.clone())),
+			PointValue::MMeTf1(m) => m_me_tf.push((ioa, m.clone())),
+			PointValue::MItTb1(m) => m_it_tb.push((ioa, m.clone())),
+			PointValue::MEpTd1(m) => m_ep_td.push((ioa, m.clone())),
+			PointValue::MEpTe1(m) => m_ep_te.push((ioa, m.clone())),
+			PointValue::MEpTf1(m) => m_ep_tf.push((ioa, m.clone())),
 		}
 	}
 
-	sort_points_by_ioa(&mut sp);
-	sort_points_by_ioa(&mut dp);
-	sort_points_by_ioa(&mut st);
-	sort_points_by_ioa(&mut bo);
-	sort_points_by_ioa(&mut mna);
-	sort_points_by_ioa(&mut mnb);
-	sort_points_by_ioa(&mut mnc);
-
-	push_interrogation_chunks(&mut out, ca, &sp, TypeId::M_SP_NA_1, InformationObjects::MSpNa1);
-	push_interrogation_chunks(&mut out, ca, &dp, TypeId::M_DP_NA_1, InformationObjects::MDpNa1);
-	push_interrogation_chunks(&mut out, ca, &st, TypeId::M_ST_NA_1, InformationObjects::MStNa1);
-	push_interrogation_chunks(&mut out, ca, &bo, TypeId::M_BO_NA_1, InformationObjects::MBoNa1);
-	push_interrogation_chunks(&mut out, ca, &mna, TypeId::M_ME_NA_1, InformationObjects::MMeNa1);
-	push_interrogation_chunks(&mut out, ca, &mnb, TypeId::M_ME_NB_1, InformationObjects::MMeNb1);
-	push_interrogation_chunks(&mut out, ca, &mnc, TypeId::M_ME_NC_1, InformationObjects::MMeNc1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_sp_na, TypeId::M_SP_NA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_sp_ta, TypeId::M_SP_TA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_dp_na, TypeId::M_DP_NA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_dp_ta, TypeId::M_DP_TA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_st_na, TypeId::M_ST_NA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_st_ta, TypeId::M_ST_TA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_bo_na, TypeId::M_BO_NA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_na, TypeId::M_ME_NA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_ta, TypeId::M_ME_TA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_nb, TypeId::M_ME_NB_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_tb, TypeId::M_ME_TB_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_nc, TypeId::M_ME_NC_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_tc, TypeId::M_ME_TC_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_it_na, TypeId::M_IT_NA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_ep_ta, TypeId::M_EP_TA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_ep_tb, TypeId::M_EP_TB_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_ep_tc, TypeId::M_EP_TC_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_ps_na, TypeId::M_PS_NA_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_nd, TypeId::M_ME_ND_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_sp_tb, TypeId::M_SP_TB_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_dp_tb, TypeId::M_DP_TB_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_st_tb, TypeId::M_ST_TB_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_bo_tb, TypeId::M_BO_TB_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_td, TypeId::M_ME_TD_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_te, TypeId::M_ME_TE_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_me_tf, TypeId::M_ME_TF_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_it_tb, TypeId::M_IT_TB_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_ep_td, TypeId::M_EP_TD_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_ep_te, TypeId::M_EP_TE_1);
+	sort_and_push_interrogation_chunks(&mut out, ca, &mut m_ep_tf, TypeId::M_EP_TF_1);
 
 	out
 }
--- src/rtu_server/point_value.rs
+++ src/rtu_server/point_value.rs
@@ -1,11 +1,17 @@
-//! RTU in-memory point value: NA monitoring payloads the simulator stores per
-//! IOA.
+//! RTU in-memory point value: monitoring payloads (Type IDs 1–40 per IEC
+//! 60870-5-104) the simulator stores per IOA.
 //!
-//! This is separate from [`crate::types::InformationObjects`]; rows here must
-//! stay aligned with the corresponding `TypeId` / wire types in the core layer.
+//! Aligned with [`crate::types_id::TypeId`] variants in the 1–40 range (gaps 8,
+//! 16). Keep the macro table in sync with [`crate::types::InformationObjects`]
+//! and with the match in `output::interrogation_data_asdus`.
 
 use crate::{
-	types::{MBoNa1, MDpNa1, MMeNa1, MMeNb1, MMeNc1, MSpNa1, MStNa1},
+	types::{
+		GenericObject, InformationObjects, MBoNa1, MBoTb1, MDpNa1, MDpTa1, MDpTb1, MEpTa1, MEpTb1,
+		MEpTc1, MEpTd1, MEpTe1, MEpTf1, MItNa1, MItTb1, MMeNa1, MMeNb1, MMeNc1, MMeNd1, MMeTa1,
+		MMeTb1, MMeTc1, MMeTd1, MMeTe1, MMeTf1, MPsNa1, MSpNa1, MSpTa1, MSpTb1, MStNa1, MStTa1,
+		MStTb1,
+	},
 	types_id::TypeId,
 };
 
@@ -25,6 +31,25 @@ macro_rules! define_rtu_point_value {
 			pub const fn type_id(&self) -> TypeId {
 				match self {
 					$( Self::$variant(_) => TypeId::$type_id, )*
+				}
+			}
+
+			/// One information object at `ioa` for spontaneous / read responses.
+			#[must_use]
+			pub fn single_object_information_objects(
+				&self,
+				ioa: u32,
+			) -> (TypeId, InformationObjects) {
+				match self {
+					$(
+						Self::$variant(m) => (
+							TypeId::$type_id,
+							InformationObjects::from(vec![GenericObject {
+								address: ioa,
+								object: m.clone(),
+							}]),
+						),
+					)*
 				}
 			}
 		}
@@ -50,10 +75,33 @@ macro_rules! define_rtu_point_value {
 
 define_rtu_point_value! {
 	M_SP_NA_1 => MSpNa1(MSpNa1) ;
+	M_SP_TA_1 => MSpTa1(MSpTa1) ;
 	M_DP_NA_1 => MDpNa1(MDpNa1) ;
+	M_DP_TA_1 => MDpTa1(MDpTa1) ;
 	M_ST_NA_1 => MStNa1(MStNa1) ;
+	M_ST_TA_1 => MStTa1(MStTa1) ;
 	M_BO_NA_1 => MBoNa1(MBoNa1) ;
 	M_ME_NA_1 => MMeNa1(MMeNa1) ;
+	M_ME_TA_1 => MMeTa1(MMeTa1) ;
 	M_ME_NB_1 => MMeNb1(MMeNb1) ;
+	M_ME_TB_1 => MMeTb1(MMeTb1) ;
 	M_ME_NC_1 => MMeNc1(MMeNc1) ;
+	M_ME_TC_1 => MMeTc1(MMeTc1) ;
+	M_IT_NA_1 => MItNa1(MItNa1) ;
+	M_EP_TA_1 => MEpTa1(MEpTa1) ;
+	M_EP_TB_1 => MEpTb1(MEpTb1) ;
+	M_EP_TC_1 => MEpTc1(MEpTc1) ;
+	M_PS_NA_1 => MPsNa1(MPsNa1) ;
+	M_ME_ND_1 => MMeNd1(MMeNd1) ;
+	M_SP_TB_1 => MSpTb1(MSpTb1) ;
+	M_DP_TB_1 => MDpTb1(MDpTb1) ;
+	M_ST_TB_1 => MStTb1(MStTb1) ;
+	M_BO_TB_1 => MBoTb1(MBoTb1) ;
+	M_ME_TD_1 => MMeTd1(MMeTd1) ;
+	M_ME_TE_1 => MMeTe1(MMeTe1) ;
+	M_ME_TF_1 => MMeTf1(MMeTf1) ;
+	M_IT_TB_1 => MItTb1(MItTb1) ;
+	M_EP_TD_1 => MEpTd1(MEpTd1) ;
+	M_EP_TE_1 => MEpTe1(MEpTe1) ;
+	M_EP_TF_1 => MEpTf1(MEpTf1) ;
 }
